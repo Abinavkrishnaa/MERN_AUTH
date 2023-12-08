46cc7482c1aceb140abefc5a22b1bf40b81c2d7f
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2023-12-08 20:26:11 +0530

    3rd commit

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express  from "express";
-
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to database");
+}).catch((err)=>{
+    console.log(err);
+})
  
 app.listen(3000,()=>{
     console.log("App is runing on port 3000!")
