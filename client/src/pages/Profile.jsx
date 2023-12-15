c656be26d3a9370595daea371689842407e4ccf1
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2023-12-15 21:57:48 +0530

    error change

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -50,7 +50,7 @@ export default function Profile() {
       <form className='flex flex-col gap-4'>
         <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e)=>setImage(e.target.files[0])} />
         <img
-          src={currentUser.profilePicture}
+          src={ formData.profilePicture ||  currentUser.profilePicture}
           alt='profile'
           className='h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2'
           onClick={()=>{
