586328d467bf7842c819ffd82f481832f0cbfd86
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2023-12-16 16:35:26 +0530

    project done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -77,3 +77,6 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+export const signout = (req, res) => {
+  res.clearCookie('access_token').status(200).json('Signout success!');
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signin, signup, google } from '../controllers/auth.controller.js';
+import { signin, signup, google, signout } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/signup", signup)
 router.post("/signin", signin)
 router.post('/google',google)
+router.get('/signout', signout);
 
 export default router;
